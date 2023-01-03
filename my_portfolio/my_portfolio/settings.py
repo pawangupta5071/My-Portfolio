58eaae348827d013f531a6eb822c398cb23ece91
pawan gupta <pawangupta5071@gmail.com>
2023-01-04 01:10:45 +0530

    Added PostgreSql

--- my_portfolio/my_portfolio/settings.py
+++ my_portfolio/my_portfolio/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'my_portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'E1uTlEni28Ka7kvMARMh',
+        'HOST': 'containers-us-west-104.railway.app',
+        'PORT': '6708',
     }
 }
 
